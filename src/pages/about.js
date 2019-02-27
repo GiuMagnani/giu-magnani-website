06e52f3479daf705aa9b71ae29e5d9f1228e851a
Giu Magnani <glmagnani@gmail.com>
2019-02-27 22:10:12 +0100

    Styling about

--- src/pages/about.js
+++ src/pages/about.js
@@ -19,36 +19,39 @@ class About extends React.Component {
         <AboutWrapper>
           <div className="container">
             <AboutInner>
-              <p>
-                I started working with small business and participating in
-                design contests to build a strong portfolio and references. With
-              </p>
-              <Highlight>Santiago, Chile</Highlight>
-              <p>
-                perseverance and hard work, in 2012 I was contacted to work
-                remotely with a company in USA which produces logo and print
-                design services. In 2013, I went
-              </p>
-              <Highlight>London, UK</Highlight>
-              <p>
-                to London, UK to find new perspectives and inspiration, where I
-                had the pleasure to work with a vintage lighting signs company
-                and a Swedish design studio. In January 2014, with more than 3
-                years of experience, I launched my new design brand, to fully
-                pursue what I love, Web Development, Design and Illustration.
-              </p>
-              <Highlight>Milan, Italy</Highlight>
-              <p>
-                Right now I'm living in Milan, Italy. I'm working as a Front-End
-                Developer, I keep learning and facing new challenges to improve
-                every day more and more.
-              </p>
+              <MainText>
+                <p>
+                  I started working with small business and participating in
+                  design contests to build a strong portfolio and references. With
+                </p>
+                <Highlight>Santiago, Chile</Highlight>
+                <p>
+                  perseverance and hard work, in 2012 I was contacted to work
+                  remotely with a company in USA which produces logo and print
+                  design services. In 2013, I went
+                </p>
+                <Highlight>London, UK</Highlight>
+                <p>
+                  to London, UK to find new perspectives and inspiration, where I
+                  had the pleasure to work with a vintage lighting signs company
+                  and a Swedish design studio. In January 2014, with more than 3
+                  years of experience, I launched my new design brand, to fully
+                  pursue what I love, Web Development, Design and Illustration.
+                </p>
+                <Highlight>Milan, Italy</Highlight>
+                <p>
+                  Right now I'm living in Milan, Italy. I'm working as a Front-End
+                  Developer, I keep learning and facing new challenges to improve
+                  every day more and more.
+                </p>
+              </MainText>
+              <MainImage>a</MainImage>
             </AboutInner>
             <Colophon>
               <div>
                 This site was built with <a>React</a>, <a>GatsbyJS</a> and{" "}
-                <a>Styled Components</a>. The font used is <a>Chakra</a> from
-                Google Fonts, designed by... etc...
+                <a>Styled Components</a>. The font used is <a>Chakra Petch</a> from
+                Google Fonts, designed by <a href="http://font.cadsondemak.com/foundry/">Cadson Demak</a>.
               </div>
             </Colophon>
           </div>
@@ -71,6 +74,7 @@ const Intro = styled.div`
   }
   .container {
     border: 1px solid ${props => props.theme.main};
+    border-top: 0;
     min-height: 50vh;
     padding: 1rem;
     display: flex;
@@ -137,32 +141,46 @@ const Colophon = styled.div`
   }
 `;
 
+const MainImage = styled.div`
+  width: 50%;
+  background: ${props => props.theme.main};
+  padding: 1rem;
+  flex: 1;
+  margin: 1rem 0 1rem 1rem;
+`;
+
+const MainText = styled.div`
+  border: 1px solid ${props => props.theme.main};
+  margin: 1rem 0;
+  padding: 2rem 1rem;
+  width: 50%;
+  flex: 1;
+`;
+
 const AboutInner = styled.div`
-  padding: 2rem 1rem 2rem 0;
   text-align: justify;
-  max-width: 590px;
-  border-right: 1px solid ${props => props.theme.main};
   position: relative;
+  display: flex;
 
   &::after {
     content: "";
     position: absolute;
-    top: -2rem;
-    left: -2rem;
-    width: 2rem;
-    height: 2rem;
+    top: 1rem;
+    left: -0.3rem;
+    width: 0.3rem;
+    height: 2.5rem;
     background-color: ${props => props.theme.main};
   }
 
-  &::before {
-    content: "";
-    position: absolute;
-    top: -2.4rem;
-    left: -2.4rem;
-    width: 2rem;
-    height: 2rem;
-    border: 1px solid ${props => props.theme.main};
-  }
+  // &::before {
+  //   content: "";
+  //   position: absolute;
+  //   top: -1.4rem;
+  //   left: -2.4rem;
+  //   width: 2rem;
+  //   height: 2rem;
+  //   border: 1px solid ${props => props.theme.main};
+  // }
 `;
 
 export default About;
