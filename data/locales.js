ac6ac50b7c91dc25ee760dfd0eb12f79506d7e66
Giu Magnani <glmagnani@gmail.com>
2019-03-11 13:40:42 +0100

    Create locales.js

---
+++ data/locales.js
@@ -0,0 +1,15 @@
+module.exports = {
+  en: {
+    path: 'en',
+    locale: 'English',
+    default: true
+  },
+  es: {
+    path: 'es',
+    locale: 'Español'
+  }
+  it: {
+    path: 'it',
+    locale: 'Italiano'
+  }
+}
